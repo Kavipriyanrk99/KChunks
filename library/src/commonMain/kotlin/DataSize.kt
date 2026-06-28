182309a4661358c67b12caa26b90407e9ad33010
kavipriyanrk99 <kavipriyanrk99@gmail.com>
2026-06-28 22:48:23 +0530

    converted units from decimal multiples (Kilobytes) to binary multiples (Kibibytes)

--- library/src/commonMain/kotlin/DataSize.kt
+++ library/src/commonMain/kotlin/DataSize.kt
@@ -1,13 +1,13 @@
 class DataSize(val bytes: Long) {
     companion object {
         const val ZERO_BYTES = 0L
-        const val BASE_BYTES = 1000L
+        const val BASE_BYTES = 1024L
         inline val Long.bytes: DataSize get() = DataSize(this)
-        inline val Long.kilobytes: DataSize get() = DataSize(this * BASE_BYTES)
-        inline val Long.megabytes: DataSize get() = DataSize(this * BASE_BYTES * BASE_BYTES)
-        inline val Long.gigabytes: DataSize get() = DataSize(this * BASE_BYTES * BASE_BYTES * BASE_BYTES)
-        inline val Long.terabytes: DataSize get() = DataSize(this * BASE_BYTES * BASE_BYTES * BASE_BYTES * BASE_BYTES)
-        inline val Long.petabytes: DataSize get() = DataSize(this * BASE_BYTES * BASE_BYTES * BASE_BYTES * BASE_BYTES * BASE_BYTES)
+        inline val Long.kibibytes: DataSize get() = DataSize(this * BASE_BYTES)
+        inline val Long.mebibytes: DataSize get() = DataSize(this * BASE_BYTES * BASE_BYTES)
+        inline val Long.gibibytes: DataSize get() = DataSize(this * BASE_BYTES * BASE_BYTES * BASE_BYTES)
+        inline val Long.tebibytes: DataSize get() = DataSize(this * BASE_BYTES * BASE_BYTES * BASE_BYTES * BASE_BYTES)
+        inline val Long.pebibytes: DataSize get() = DataSize(this * BASE_BYTES * BASE_BYTES * BASE_BYTES * BASE_BYTES * BASE_BYTES)
     }
 
     fun toLong() = bytes

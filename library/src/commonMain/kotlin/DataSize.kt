ae2f6608deaaa1e307bbb7310bb369403d96640f
kavipriyanrk99 <kavipriyanrk99@gmail.com>
2026-07-03 13:02:49 +0530

    modified DataSize unit class by adding overflow validations

--- library/src/commonMain/kotlin/DataSize.kt
+++ library/src/commonMain/kotlin/DataSize.kt
@@ -1,21 +1,73 @@
-class DataSize(val bytes: Long) {
+import kotlin.math.roundToLong
+
+/**
+ * Represents a non-negative quantity of data.
+ *
+ * Internally, all values are stored as a [Long] number of bytes. Since bytes are
+ * the smallest supported unit, the largest representable value is [Long.MAX_VALUE]
+ * bytes.
+ *
+ * Factory extensions such as `Long.kibibytes` and `Double.gibibytes` perform
+ * overflow checks before creating a [DataSize]. Any value that would exceed
+ * [Long.MAX_VALUE] bytes results in an [IllegalArgumentException].
+ */
+class DataSize(val bytes: Long): Comparable<DataSize> {
     companion object {
-        const val ZERO_BYTES = 0L
-        const val BASE_BYTES = 1024L
-        inline val Long.bytes: DataSize get() = DataSize(this)
-        inline val Long.kibibytes: DataSize get() = DataSize(this * BASE_BYTES)
-        inline val Long.mebibytes: DataSize get() = DataSize(this * BASE_BYTES * BASE_BYTES)
-        inline val Long.gibibytes: DataSize get() = DataSize(this * BASE_BYTES * BASE_BYTES * BASE_BYTES)
-        inline val Long.tebibytes: DataSize get() = DataSize(this * BASE_BYTES * BASE_BYTES * BASE_BYTES * BASE_BYTES)
-        inline val Long.pebibytes: DataSize get() = DataSize(this * BASE_BYTES * BASE_BYTES * BASE_BYTES * BASE_BYTES * BASE_BYTES)
+        internal const val ZERO_BYTES = 0L
+        internal const val KIBIBYTES = 1024L
+        internal const val MEBIBYTES = KIBIBYTES * KIBIBYTES
+        internal const val GIBIBYTES = MEBIBYTES * KIBIBYTES
+        internal const val TEBIBYTES = GIBIBYTES * KIBIBYTES
+        internal const val PEBIBYTES = TEBIBYTES * KIBIBYTES
+        internal const val MAX_BYTES = Long.MAX_VALUE
+        internal const val MAX_KIBIBYTES = MAX_BYTES / KIBIBYTES
+        internal const val MAX_MEBIBYTES = MAX_BYTES / MEBIBYTES
+        internal const val MAX_GIBIBYTES = MAX_BYTES / GIBIBYTES
+        internal const val MAX_TEBIBYTES = MAX_BYTES / TEBIBYTES
+        internal const val MAX_PEBIBYTES = MAX_BYTES / PEBIBYTES
+
+        private fun Long.toDataSize(unit: Long, max: Long): DataSize {
+            require(this >= 0) { "DataSize can't be negative" }
+            require(this <= max) { "DataSize overflow. DataSize handles only a maximum size of $MAX_BYTES bytes" }
+            return DataSize(this * unit)
+        }
+
+        private fun Double.toDataSize(unit: Long): DataSize {
+            val bytes = this * unit
+            require(bytes.isFinite()) { "DataSize should be finite" }
+            require(bytes >= 0) { "DataSize can't be negative" }
+            require(bytes <= MAX_BYTES.toDouble()) { "DataSize overflow. DataSize handles only a maximum size of $MAX_BYTES bytes" }
+            return DataSize(bytes.roundToLong())
+        }
+
+        val Long.bytes: DataSize get() = toDataSize(1, MAX_BYTES)
+
+        val Long.kibibytes: DataSize get() = toDataSize(KIBIBYTES, MAX_KIBIBYTES)
+
+        val Double.kibibytes: DataSize get() = toDataSize(KIBIBYTES)
+
+        val Long.mebibytes: DataSize get() = toDataSize(MEBIBYTES, MAX_MEBIBYTES)
+
+        val Double.mebibytes: DataSize get() = toDataSize(MEBIBYTES)
+
+        val Long.gibibytes: DataSize get() = toDataSize(GIBIBYTES, MAX_GIBIBYTES)
+
+        val Double.gibibytes: DataSize get() = toDataSize(GIBIBYTES)
+
+        val Long.tebibytes: DataSize get() = toDataSize(TEBIBYTES, MAX_TEBIBYTES)
+
+        val Double.tebibytes: DataSize get() = toDataSize(TEBIBYTES)
+
+        val Long.pebibytes: DataSize get() = toDataSize(PEBIBYTES, MAX_PEBIBYTES)
+
+        val Double.pebibytes: DataSize get() = toDataSize(PEBIBYTES)
     }
 
-    fun toLong() = bytes
-    fun equalTo(other: DataSize) = this.bytes == other.bytes
-    fun lessThanOrEqualTo(other: DataSize) = this.bytes <= other.bytes
-    fun lessThan(other: DataSize) = this.bytes < other.bytes
-    fun greaterThan(other: DataSize) = this.bytes > other.bytes
-    fun greaterThanOrEqualTo(other: DataSize) = this.bytes >= other.bytes
-
     override fun toString(): String = bytes.toString()
+
+    override fun compareTo(other: DataSize): Int = when {
+        this.bytes == other.bytes -> 0
+        this.bytes < other.bytes -> -1
+        else -> 1
+    }
 }

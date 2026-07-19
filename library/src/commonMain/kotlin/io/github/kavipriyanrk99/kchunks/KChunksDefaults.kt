41d4b022506097347e4a18de8f136546c3effcfb
kavipriyanrk99 <kavipriyanrk99@gmail.com>
2026-07-19 12:18:29 +0530

    optimized defaults like buffersize, no of chunks & default timeout

--- library/src/commonMain/kotlin/io/github/kavipriyanrk99/kchunks/KChunksDefaults.kt
+++ library/src/commonMain/kotlin/io/github/kavipriyanrk99/kchunks/KChunksDefaults.kt
@@ -16,11 +16,11 @@ internal object KChunksDefaults {
     }
 
     const val DEFAULT_WORKERS = 4
-    const val DEFAULT_NO_OF_CHUNKS = 16
+    const val DEFAULT_NO_OF_CHUNKS = 32
     const val DEFAULT_CHUNK_SIZE = 1024 * 1024 * 2L // 2 MebiBytes
-    const val DEFAULT_BUFFER_SIZE = 1024 * 16L // 16 KibiBytes
+    const val DEFAULT_BUFFER_SIZE = 1024 * 64L // 64 KibiBytes
     const val DEFAULT_SAMPLE_SIZE = 10
-    const val CONCURRENCY_LIMIT_DEFAULT_TIMEOUT_NS = 5_00_00_00_000 // 5 seconds
+    const val CONCURRENCY_LIMIT_DEFAULT_TIMEOUT_NS = 10_00_00_000 // 100 milliseconds
     const val CONCURRENCY_LIMIT_MIN_LIMIT = 2
     const val CONCURRENCY_LIMIT_INITIAL_LIMIT = 2
     const val CONCURRENCY_LIMIT_MAX_LIMIT = 32
